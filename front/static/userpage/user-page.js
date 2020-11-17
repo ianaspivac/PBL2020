d09b9cf08e743cb51cb097327c029a13c5259377
Iana Spivac <yspivac@gmail.com>
2020-11-17 23:14:57 +0200

    added use-page

--- front/static/userpage/user-page.js
+++ front/static/userpage/user-page.js
@@ -6,16 +6,11 @@ var recomended_quality = 100;
 var value_hours = 45; //orele dormite de utilizator intr-o saptamana
 var value_quality = 60; //calitatea somnului de la 0 la 100
 
-//Denumirea riscurilor
 
-var risk1 = "Risk1";
-var risk2 = "Risk2";
-var risk3 = "Risk3";
 
 //Cat e de mare riscul in cifre
 var risk_number_1= "2";
-var risk_number_2= "1";
-var risk_number_3= "1";
+document.getElementById("risk-number-1").innerHTML = risk_number_1 +"/100";
 
 //plot data
 let data1 = [1, 2, 10, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -88,13 +83,7 @@ bar2.text.style.fontSize = '2.5em';
 
 bar2.animate(value_quality);
 
-//addinf text into risks section
-document.getElementById("risk1").innerHTML = risk1;
-document.getElementById("risk2").innerHTML = risk2;
-document.getElementById("risk3").innerHTML = risk3;
-document.getElementById("risk-number-1").innerHTML = risk_number_1;
-document.getElementById("risk-number-2").innerHTML = risk_number_2;
-document.getElementById("risk-number-3").innerHTML = risk_number_3;
+
 
 //plot manipulation
 
@@ -264,4 +253,31 @@ new Chart(document.getElementById("line-chart4"), {
   },
   options: {},
 });
+
+
+var d = new Date();
+var n = d.getDay();
+var m = d.getMonth();
+var y = d.getFullYear();
+var date = d.getDate();
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dates = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+document.getElementById("month-year").innerHTML = months[m] + " " + y;
+document.getElementById("day1").innerHTML = weekdays[n];
+document.getElementById("day2").innerHTML = weekdays[n + 1];
+document.getElementById("day3").innerHTML = weekdays[n + 2];
+document.getElementById("day4").innerHTML = weekdays[n + 3];
+document.getElementById("day5").innerHTML = weekdays[n + 4];
+document.getElementById("day6").innerHTML = weekdays[n + 5];
+document.getElementById("day7").innerHTML = weekdays[n + 6];
+
+
+document.getElementById("date1").innerHTML = dates[date];
+document.getElementById("date2").innerHTML = dates[date + 1];
+document.getElementById("date3").innerHTML = dates[date + 2];
+document.getElementById("date4").innerHTML = dates[date + 3];
+document.getElementById("date5").innerHTML = dates[date + 4];
+document.getElementById("date6").innerHTML = dates[date + 5];
+document.getElementById("date7").innerHTML = dates[date + 6];
 
