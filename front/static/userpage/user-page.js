d00fba1f17bc4160eb09e7ae7fa5d1243622f4f2
Iana Spivac <yspivac@gmail.com>
2020-11-26 18:34:44 +0200

    added footer and improved user page

--- front/static/userpage/user-page.js
+++ front/static/userpage/user-page.js
@@ -122,8 +122,8 @@ new Chart(document.getElementById("line-chart1"), {
           data1[10],
           data1[11],
         ],
-        label: "Hours slept",
-        borderColor: "#3e95cd",
+        label: "Hours",
+        borderColor: "#75C0D8",
         fill: false,
       },
     ],
@@ -164,7 +164,7 @@ new Chart(document.getElementById("line-chart2"), {
           data2[11],
         ],
         label: "Hours slept",
-        borderColor: "#3e95cd",
+        borderColor: "#75C0D8",
         fill: false,
       },
     ],
@@ -205,64 +205,36 @@ new Chart(document.getElementById("line-chart3"), {
           data3[11],
         ],
         label: "Hours slept",
-        borderColor: "#3e95cd",
+        borderColor: "#75C0D8",
         fill: false,
       },
     ],
   },
   options: {},
 });
-new Chart(document.getElementById("line-chart4"), {
-  type: "line",
-  data: {
-    labels: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    datasets: [
-      {
-        data: [
-          data4[0],
-          data4[1],
-          data4[2],
-          data4[3],
-          data4[4],
-          data4[5],
-          data4[6],
-          data4[7],
-          data4[8],
-          data4[9],
-          data4[10],
-          data4[11],
-        ],
-        label: "Hours slept",
-        borderColor: "#3e95cd",
-        fill: false,
-      },
-    ],
-  },
-  options: {},
-});
+
 
 
 var d = new Date();
+var startDate = new Date();
 var n = d.getDay();
 var m = d.getMonth();
 var y = d.getFullYear();
-var date = d.getDate();
+
+
+function GetDates(startDate, daysToAdd) {
+  var dates = [];
+
+  for (var i = 0; i <= daysToAdd; i++) {
+      var currentDate = new Date();
+      currentDate.getDate(currentDate.setDate(startDate.getDate() + i));
+      dates.push(currentDate.getDate());
+}
+
+  return dates;
+}
 var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var dates = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
 document.getElementById("month-year").innerHTML = months[m] + " " + y;
 document.getElementById("day1").innerHTML = weekdays[n];
 document.getElementById("day2").innerHTML = weekdays[n + 1];
@@ -273,11 +245,15 @@ document.getElementById("day6").innerHTML = weekdays[n + 5];
 document.getElementById("day7").innerHTML = weekdays[n + 6];
 
 
-document.getElementById("date1").innerHTML = dates[date];
-document.getElementById("date2").innerHTML = dates[date + 1];
-document.getElementById("date3").innerHTML = dates[date + 2];
-document.getElementById("date4").innerHTML = dates[date + 3];
-document.getElementById("date5").innerHTML = dates[date + 4];
-document.getElementById("date6").innerHTML = dates[date + 5];
-document.getElementById("date7").innerHTML = dates[date + 6];
+var dates = GetDates(startDate, 7);
+document.getElementById("date1").innerHTML = dates[0];
+document.getElementById("date2").innerHTML = dates[1];
+document.getElementById("date3").innerHTML = dates[2];
+document.getElementById("date4").innerHTML = dates[3];
+document.getElementById("date5").innerHTML = dates[4];
+document.getElementById("date6").innerHTML = dates[5];
+document.getElementById("date7").innerHTML = dates[6];
+
+document.getElementById("sleep-time").title=document.getElementById("sleep-time").innerHTML;
+console.log(document.getElementById("3day").innerHTML);
 
